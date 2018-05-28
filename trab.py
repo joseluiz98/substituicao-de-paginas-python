57513813fb61910de81f108d531951d56492da5b
José Luiz do Carmo Silva <joseluiz98.jl@gmail.com>
2018-05-28 19:39:04 -0300

    Implementando OTM inicial

--- trab.py
+++ trab.py
@@ -3,6 +3,7 @@ tamMemoria = input()
 acessos = []
 memoriaFIFO = []
 memoriaLRU = []
+memoriaOTM = []
 pageMiss = [0,0,0] #pageMiss[0] representa FIFO, pageMiss[1] OTM e pageMiss[2] LRU
 existe = False
 
@@ -25,6 +26,41 @@ for acesso in acessos:
         pageMiss[0] = pageMiss[0] + 1
     existe = False
 
+def proximaUtilizacao(elemento, acessosAMemoria, elementosJaAnalisados):
+    "Procura a próxima utilização do elemento recebido como parâmetro"
+    "Parâmetros: elemento a ser buscada próxima utilização, vetor contendo acessos à memória"
+    "Retorna: int contendo momento da próxima utilização"
+    indice = elementosJaAnalisados # Índice começa pelo primeiro elemento não analisado
+    acessosAMemoria = acessosAMemoria[elementosJaAnalisados:] # Ponto de partida, excluindo elementos cujos acessos já foram tratados
+    for acesso in acessosAMemoria:
+        if(acesso == elemento): return indice # Próxima referência no futuro encontrada, retorne-a
+        indice = indice + 1
+
+    return -1
+
+# Algoritmo OTM (Roda antes do LRU pois este último altera o vetor de acessos!)
+contador = 0
+referenciasFuturas = []
+for acesso in acessos:
+    for elemento in memoriaOTM:
+        if(elemento == acesso): existe = True
+    if(not(existe)):
+        if(len(memoriaOTM) != tamMemoria):
+            memoriaOTM.append(acesso)
+        else: # Se memória cheia, e elemento não está nela, remove o que será referenciado o mais tarde possível
+            for elemento in memoriaOTM:
+                referenciasFuturas.append(proximaUtilizacao(elemento,acessos,contador)) # Procura referencias futuras de cada elemento e guarda em vetor
+            referenciaMaisDistante = 0
+            for referencia in referenciasFuturas:
+                if(referencia > referenciaMaisDistante): referenciaMaisDistante = referencia # Das referências, procura a mais futura possível
+            elemento = memoriaOTM.index(referenciaMaisDistante)
+            memoriaOTM.pop(elemento)
+            memoriaOTM.append(acesso)
+        pageMiss[1] = pageMiss[1] + 1
+    existe = False
+    contador = contador + 1
+    referenciasFuturas = []
+
 # Algoritmo LRU
 momento = 0 # Representa o momento em que o acesso ocorre, nesta implementação, o loop do for, ou seja, quanto menor este número, mais antigo foi o acesso ao dado
 
@@ -43,7 +79,7 @@ for acesso in acessos:
             elemento = acesso
             memoriaLRU.append(acesso)
         else:
-            menosRecente = memoriaLRU[0][1]
+            menosRecente = memoriaLRU[0][1] 
             for elemento in memoriaLRU:
                 if(elemento[1] < menosRecente): menosRecente = elemento # Procura o elemento menos recentemente utilizado
             elemento = memoriaLRU.index(elemento) # Recebe o índice do elemento
@@ -56,4 +92,5 @@ for acesso in acessos:
     momento = momento+1
 
 print pageMiss[0]
+print pageMiss[1]
 print pageMiss[2]
