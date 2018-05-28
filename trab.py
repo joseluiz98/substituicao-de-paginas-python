20cb8f27e3f2e4f21281439f35c7cfc1bc764b54
José Luiz do Carmo Silva <joseluiz98.jl@gmail.com>
2018-05-28 11:15:22 -0300

    Memória seguindo principio da fila implementada

--- trab.py
+++ trab.py
@@ -1,2 +1,22 @@
-a = raw_input()
-print a
+tamMemoria = input()
+
+i = 1
+memoria = []
+pageMiss = 0
+existe = False
+elemAInserir = 0
+elemAInserir = input()
+while(elemAInserir != -1):
+    for elemento in memoria:
+        if(elemento == elemAInserir): existe = True
+    if(existe == False):
+        if(len(memoria) != tamMemoria):
+            memoria.append(elemAInserir) # Insere no fim da memória, caso haja espaço
+        else:
+            memoria.pop(0) # Remove no início da memória, caso ela esteja cheia
+            memoria.append(elemAInserir)
+        pageMiss = pageMiss + 1
+    i = i+1
+    existe = False
+    elemAInserir = input()
+print pageMiss
